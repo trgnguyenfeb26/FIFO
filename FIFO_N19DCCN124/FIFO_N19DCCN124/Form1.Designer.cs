024bd39fe1a710fac622a2ddb0a7e8b73970a4e9
Trung Nguyên <nguyennguyentrung01@gmail.com>
2022-04-23 04:55:50 +0700

    upload data

---
+++ FIFO_N19DCCN124/FIFO_N19DCCN124/Form1.Designer.cs
@@ -0,0 +1,264 @@
+﻿
+namespace FIFO_N19DCCN124
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btn_Run = new System.Windows.Forms.Button();
+            this.btn_Reset = new System.Windows.Forms.Button();
+            this.tb_Screen = new System.Windows.Forms.TextBox();
+            this.tb_Tieude = new System.Windows.Forms.TextBox();
+            this.at_Sotrang = new System.Windows.Forms.TextBox();
+            this.at_Sokhungtrang = new System.Windows.Forms.TextBox();
+            this.at_Chuoitrang = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tb_ThongBao = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btn_Run
+            // 
+            this.btn_Run.BackColor = System.Drawing.Color.DodgerBlue;
+            this.btn_Run.Location = new System.Drawing.Point(959, 24);
+            this.btn_Run.Name = "btn_Run";
+            this.btn_Run.Size = new System.Drawing.Size(160, 68);
+            this.btn_Run.TabIndex = 0;
+            this.btn_Run.Text = "Chạy chương trình";
+            this.btn_Run.UseVisualStyleBackColor = false;
+            this.btn_Run.Click += new System.EventHandler(this.btn_Run_Click);
+            // 
+            // btn_Reset
+            // 
+            this.btn_Reset.BackColor = System.Drawing.Color.DodgerBlue;
+            this.btn_Reset.Location = new System.Drawing.Point(959, 114);
+            this.btn_Reset.Name = "btn_Reset";
+            this.btn_Reset.Size = new System.Drawing.Size(160, 68);
+            this.btn_Reset.TabIndex = 1;
+            this.btn_Reset.Text = "Đặt lại";
+            this.btn_Reset.UseVisualStyleBackColor = false;
+            this.btn_Reset.Click += new System.EventHandler(this.btn_Reset_Click);
+            // 
+            // tb_Screen
+            // 
+            this.tb_Screen.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.tb_Screen.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.tb_Screen.Location = new System.Drawing.Point(4, 24);
+            this.tb_Screen.Margin = new System.Windows.Forms.Padding(4);
+            this.tb_Screen.Multiline = true;
+            this.tb_Screen.Name = "tb_Screen";
+            this.tb_Screen.ReadOnly = true;
+            this.tb_Screen.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.tb_Screen.Size = new System.Drawing.Size(915, 289);
+            this.tb_Screen.TabIndex = 9;
+            this.tb_Screen.WordWrap = false;
+            // 
+            // tb_Tieude
+            // 
+            this.tb_Tieude.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.tb_Tieude.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.tb_Tieude.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.tb_Tieude.Location = new System.Drawing.Point(4, 2);
+            this.tb_Tieude.Margin = new System.Windows.Forms.Padding(4);
+            this.tb_Tieude.Name = "tb_Tieude";
+            this.tb_Tieude.ReadOnly = true;
+            this.tb_Tieude.Size = new System.Drawing.Size(891, 26);
+            this.tb_Tieude.TabIndex = 10;
+            this.tb_Tieude.Text = "Thuật toán thay thế trang FIPO\r\n";
+            this.tb_Tieude.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.tb_Tieude.TextChanged += new System.EventHandler(this.tb_Tieude_TextChanged);
+            // 
+            // at_Sotrang
+            // 
+            this.at_Sotrang.Location = new System.Drawing.Point(712, 377);
+            this.at_Sotrang.Margin = new System.Windows.Forms.Padding(4);
+            this.at_Sotrang.Multiline = true;
+            this.at_Sotrang.Name = "at_Sotrang";
+            this.at_Sotrang.Size = new System.Drawing.Size(207, 22);
+            this.at_Sotrang.TabIndex = 11;
+            this.at_Sotrang.Tag = "";
+            this.at_Sotrang.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // at_Sokhungtrang
+            // 
+            this.at_Sokhungtrang.Location = new System.Drawing.Point(351, 377);
+            this.at_Sokhungtrang.Margin = new System.Windows.Forms.Padding(4);
+            this.at_Sokhungtrang.Name = "at_Sokhungtrang";
+            this.at_Sokhungtrang.Size = new System.Drawing.Size(185, 22);
+            this.at_Sokhungtrang.TabIndex = 12;
+            this.at_Sokhungtrang.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.at_Sokhungtrang.TextChanged += new System.EventHandler(this.at_Sokhungtrang_TextChanged);
+            // 
+            // at_Chuoitrang
+            // 
+            this.at_Chuoitrang.Location = new System.Drawing.Point(321, 409);
+            this.at_Chuoitrang.Margin = new System.Windows.Forms.Padding(4);
+            this.at_Chuoitrang.Name = "at_Chuoitrang";
+            this.at_Chuoitrang.Size = new System.Drawing.Size(594, 22);
+            this.at_Chuoitrang.TabIndex = 13;
+            this.at_Chuoitrang.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.label1.Location = new System.Drawing.Point(926, 202);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(215, 60);
+            this.label1.TabIndex = 14;
+            this.label1.Text = "Giảng viên: Huỳnh Thanh Tâm\r\nSinh viên: Nguyễn Trung Nguyên\r\nMSSV: N19DCCN124\r\nMô" +
+    "n: Hệ Điều Hành";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label1.Click += new System.EventHandler(this.label1_Click_1);
+            // 
+            // tb_ThongBao
+            // 
+            this.tb_ThongBao.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.tb_ThongBao.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.tb_ThongBao.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tb_ThongBao.Location = new System.Drawing.Point(160, 330);
+            this.tb_ThongBao.Margin = new System.Windows.Forms.Padding(4);
+            this.tb_ThongBao.Name = "tb_ThongBao";
+            this.tb_ThongBao.ReadOnly = true;
+            this.tb_ThongBao.Size = new System.Drawing.Size(755, 30);
+            this.tb_ThongBao.TabIndex = 15;
+            this.tb_ThongBao.TextChanged += new System.EventHandler(this.tb_ThongBao_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.label2.Location = new System.Drawing.Point(12, 331);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(130, 29);
+            this.label2.TabIndex = 16;
+            this.label2.Text = "Thông báo";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.label3.Location = new System.Drawing.Point(156, 378);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(163, 20);
+            this.label3.TabIndex = 17;
+            this.label3.Text = "Nhập số khung trang";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.label4.Location = new System.Drawing.Point(567, 378);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(114, 20);
+            this.label4.TabIndex = 18;
+            this.label4.Text = "Nhập số trang";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.label5.Location = new System.Drawing.Point(156, 410);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(136, 20);
+            this.label5.TabIndex = 19;
+            this.label5.Text = "Nhập chuỗi trang";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.InitialImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.InitialImage")));
+            this.pictureBox1.Location = new System.Drawing.Point(959, 295);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(173, 135);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 20;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1182, 461);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tb_ThongBao);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.at_Chuoitrang);
+            this.Controls.Add(this.at_Sotrang);
+            this.Controls.Add(this.at_Sokhungtrang);
+            this.Controls.Add(this.tb_Tieude);
+            this.Controls.Add(this.tb_Screen);
+            this.Controls.Add(this.btn_Reset);
+            this.Controls.Add(this.btn_Run);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_Run;
+        private System.Windows.Forms.Button btn_Reset;
+        private System.Windows.Forms.TextBox tb_Screen;
+        private System.Windows.Forms.TextBox tb_Tieude;
+        private System.Windows.Forms.TextBox at_Sokhungtrang;
+        private System.Windows.Forms.TextBox at_Sotrang;
+        private System.Windows.Forms.TextBox at_Chuoitrang;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tb_ThongBao;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ FIFO_N19DCCN124/FIFO_N19DCCN124/Form1.cs
@@ -0,0 +1,256 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FIFO_N19DCCN124
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public int[] khungTrang = new int[1000];
+        public int[] chuoiTrang = new int[1000];
+        public int soKhungTrang, n, dem = 0;
+        public int[,] ketQua = new int[100, 1000];
+        public char[] loiTrang = new char[1000];
+        public bool tiepTuc;
+        //Hàm khởi tạo đặt lại biến đếm số trang lỗi (dem) = 0
+        //đặt lại màn hình và thanh thông báo
+        void khoitao()
+        {
+            dem = 0;
+            tb_Screen.ResetText();
+            tb_ThongBao.ResetText();
+           
+        }
+
+        //Hàm thuật toán FIFO
+        void FIFO()
+        {
+            int i, j, k, kiemTra;
+            //khởi tạo giá trị ban đầu cho mảng khungTrang là -1;
+            for (i = 0; i < soKhungTrang; i++) khungTrang[i] = -1;
+            //Biến j là biến thứ tự gán vào của khungTrang (ban đầu j=0) 
+            j = 0;
+            //Duyệt từ đầu đến cuối của chuỗi trang nhập vào
+            for (i = 1; i <= n; i++){
+                kiemTra = 0;
+                //Duyệt xem có khungTrang nào trùng với phần tử chuoiTrang hiện tại
+                for (k = 0; k < soKhungTrang; k++) 
+                    //Nếu trùng thì gán biến kiemTra = 1 để làm dấu
+                    if (khungTrang[k] == chuoiTrang[i]) kiemTra = 1;
+                //Nếu biến kiemTra == 0 tức là chuoiTrang hiện tại không trùng 
+                //với phần tử nào của khungTrang
+                if (kiemTra == 0){
+                    //Cập nhật khungTrang thứ j bằng chuoiTrang hiện tại
+                    khungTrang[j] = chuoiTrang[i];
+                    //Đồng thời cập nhật loiTrang hiện tại = '*'
+                    loiTrang[i] = '*';
+                    //Vì j chỉ chạy từ 0 đến soKhungTrang - 1
+                    //Nên chỉ lấy phần dư của (j + 1) / soKhungTrang
+                    j = (j + 1) % soKhungTrang;
+                    //Tăng biến đếm (dem) lỗi trang lên 1
+                    dem++;
+                    //Gán sokhungTrang hiện tại vào mảng hai chiều ketQua
+                    for (k = 0; k < soKhungTrang; k++) 
+                        ketQua[k, i] = khungTrang[k];
+                }
+                //Ngược lại là chuoiTrang hiện tại trùng với
+                //tất cả phần từ trong khungTrang
+                else
+                {
+                    //Cập nhận loiTrang hiện tại bằng trống (' ')
+                    loiTrang[i] = ' ';
+                    //Cập nhật soKhungTrang trước đó vào ketQua
+                    for (k = 0; k < soKhungTrang; k++)
+                        ketQua[k, i] = khungTrang[k];
+                }
+
+            }
+        }
+       
+        //Hàm kiểm tra Text có phải là chuỗi số hay không?
+        bool KiemTraSo(string Text)
+        {
+            //Nếu chuỗi Text rỗng thì chuỗi không phải là chuỗi số
+            if (Text == "") return false;
+            //Duyệt từng phần tử trong Text 
+            foreach (Char c in Text)
+            {
+                //Nếu có 1 phần từ không phải là số thì Text không phải là chuỗi số
+                if (!Char.IsDigit(c))
+                    return false;
+            }
+            //Nếu tất cả phần tử là số thì Text là chuỗi số
+            return true;
+        }
+        //Hàm kiểm tra ChuoiTrang có phải là mảng số cách nhau bởi ' '
+        //ví dụ "3 4 6 2 3 4 5 6 6" ->true
+        bool KiemTraChuoiTrang(string pText)
+        {
+            //Nếu chuỗi pText rỗng thì chuỗi không phải là chuỗi số
+            if (pText == "") return false;
+            //Duyệt từng phần tử trong pText 
+            foreach (Char c in pText)
+            {
+                //Nếu có 1 phần từ không phải là số và khác ' '
+                //thì pText không phải là chuỗi số
+                if (!Char.IsDigit(c) && c != ' ')
+                    return false;
+            }
+            return true;
+        }
+        //Hàm đọc dữ liệu từ người dùng nhập
+        void DocDuLieu()
+        { 
+            //Khai báo mảng chuỗi tam 
+            string[] tam = null;
+            //Gán từ phần tử của mảng chuỗi tạm bằng
+            //những số được tách từ chuỗi trang với dấu hiệu để tách là ' '
+            //Ví dụ: "3 4 5 6 4" sẽ được tách thành "3","4","5","6","4" 
+            tam = new Regex(" ").Split(at_Chuoitrang.Text);
+            //Kiểu tra xem người dùng có nhập đúng dữ kiệu hay không
+            if (KiemTraSo(at_Sotrang.Text)==false || KiemTraSo(at_Sokhungtrang.Text)==false || KiemTraChuoiTrang(at_Chuoitrang.Text)==false)
+            {
+                //Nếu không thì thông báo lỗi
+                MessageBox.Show("Chưa nhập dữ liệu hoặc nhập dữ liệu sai!", "Thông báo!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                //Cập nhật biến tiepTuc = false 
+                tiepTuc = false;
+            }
+            //Nếu người dùng nhập đúng
+            else
+            {
+                //Cập nhật biến tiepTuc = true
+                tiepTuc = true;
+                //Gán n bằng số trang mà người dùng nhập vào
+                n = int.Parse(at_Sotrang.Text.ToString());
+                //Gán soKhungTrang bằng số khung trang mà người dùng nhập vào
+                soKhungTrang = int.Parse(at_Sokhungtrang.Text);
+                //Gán tất cả phần tử trong mảng chuỗi tam vào mảng chuoiTrang
+                for (int i = 1; i <= n; i++)
+                    //int.Parse dùng để ép kiểu chuỗi thành kiểu số nguyên
+                    chuoiTrang[i] = int.Parse(tam[i - 1]);
+                
+            }
+        }
+        //Hàm thực hiện thuật toán FIFO và hiển thị dữ liệu ra màn hình 
+        void ThucHien()
+        {
+            //Gọi hàm FIFO() để chạy thuật toán FIFO
+            FIFO();
+            //Cập nhật màn hình thành rỗng
+            tb_Screen.Text = "";
+            //Duyệt mảng hai chiều ketQua 
+            //Chỉ số i chạy từ 0 đến số khung trang - 1
+            //Chỉ số j chạy từ 1 đến n (số trang)
+            for (int i = 0; i < soKhungTrang; i++){
+                for (int j = 1; j <= n; j++)
+                    //Nếu giá trị của ketQua hiện tại khác -1
+                    if (ketQua[i, j] != -1)
+                        //Thì hiển thị ra màn hình kết quả hiện tại
+                        //"\t" là tab để dễ dàng phân biệt các phần tử
+                        tb_Screen.Text += ketQua[i, j].ToString() + "\t";
+                    //Ngược lại nếu ketQua hiện tại bằng -1
+                    else
+                        //Hiển thị ra màn hình tab (trống)
+                        tb_Screen.Text += "\t";
+                //tạo một dòng mới (khung trang thứ i+1)
+                tb_Screen.Text += Environment.NewLine;
+            }
+            //Duyệt mảng loiTrang
+            for (int i = 1; i <= n; i++)
+                //Hiển thị ra màn hình dấu * nếu trang lỗi, rỗng (' ') nếu ngược lại
+                tb_Screen.Text += loiTrang[i].ToString() + "\t";
+            //Hiển thị tổng số trang lỗi
+            tb_ThongBao.Text += "Số trang lỗi là: ";
+            tb_ThongBao.Text += dem.ToString();
+        }
+        //Sự kiện nút Chạy chương trình
+        private void btn_Run_Click(object sender, EventArgs e)
+        {
+            //Khởi tạo và đọc dự liệu từ người dùng
+            khoitao();
+            DocDuLieu();
+            //Nếu người dùng nhập dữ liệu đúng thì biến tiepTuc = true 
+            if (tiepTuc == true)
+            {
+                //Chạy hàm ThucHien() 
+                ThucHien();
+            }
+            //Ngược lại người dùng nhập dữ liệu lỗi thì biến tiepTuc = false
+            else
+                //Chạy hàm Reset()
+                Reset();
+        }
+        //Hàm khởi tạo và đặt lại tất cả các giá trị về ban đầu
+        void Reset()
+        {
+            //Chạy hàm khởi tạo
+            khoitao();
+            //Đặt lại tất cả các hiển thi, edittext trên màn hình
+            tb_Screen.ResetText();
+            tb_ThongBao.ResetText();
+            at_Sotrang.ResetText();
+            at_Sokhungtrang.ResetText();
+            at_Chuoitrang.ResetText();
+        }
+        //Sự kiện nút Đặt lại
+        private void btn_Reset_Click(object sender, EventArgs e)
+        {   
+            //Chạy hàm Reset()
+            Reset();
+        }
+        private void tb_Tieude_TextChanged(object sender, EventArgs e)
+        {
+            this.CenterToScreen();
+        }
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void at_Sokhungtrang_TextChanged(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void tb_ThongBao_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        } 
+    }
+}
